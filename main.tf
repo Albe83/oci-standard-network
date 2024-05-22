9a5de3ce3629fc3e27ec8b010028167310112210
Alberto Clemente <alberto.clemente@sisal.it>
2024-05-22 19:52:47 +0200

    public net

--- main.tf
+++ main.tf
@@ -14,9 +14,14 @@ locals {
 
   compartment = data.oci_identity_compartment.compartment
   vcn = oci_core_vcn.vcn
+
   igw = oci_core_internet_gateway.igw
   ngw = oci_core_nat_gateway.ngw
   sgw = oci_core_service_gateway.sgw
+
+  rt-public = oci_core_route_table.public
+
+  net-public = oci_core_subnet.public
 }
 
 data "oci_identity_compartment" "compartment" {
@@ -52,4 +57,25 @@ resource "oci_core_service_gateway" "sgw" {
     services {
         service_id = data.oci_core_services.oci_services.services.0.id
     }
+}
+
+resource "oci_core_route_table" "public" {
+    vcn_id = local.vcn.id
+    compartment_id = local.vcn.compartment_id
+
+    route_rules {
+        description = "Default route"
+        network_entity_id = local.igw.id
+    }
+}
+
+resource "oci_core_subnet" "public" {
+    vcn_id = local.vcn.id
+    compartment_id = local.vcn.compartment_id
+
+    cidr_block = local.public_cidr
+    prohibit_internet_ingress = false
+    prohibit_public_ip_on_vnic = false
+
+    route_table_id = local.rt-public.id
 }
