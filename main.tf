6aba79ef042408271e6b02bd0c8fcaeab69a0ad5
Alberto Clemente <alberto.clemente@sisal.it>
2024-05-22 19:35:49 +0200

    vcn

--- main.tf
+++ main.tf
@@ -9,10 +9,22 @@ terraform {
 
 locals {
   compartment_id = var.compartment_id
+  public_cidr = "192.168.0.0/24"
+  private_cidr = "192.168.100.0/24"
 
   compartment = data.oci_identity_compartment.compartment
+  vcn = oci_core_vcn.vcn
 }
 
 data "oci_identity_compartment" "compartment" {
     id = local.compartment_id
+}
+
+resource "oci_core_vcn" "vcn" {
+    compartment_id = local.compartment.id
+
+    cidr_blocks = [
+        local.public_cidr,
+        local.private_cidr
+    ]
 }
