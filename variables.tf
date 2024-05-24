e1d85031283c8bd620929179e5de2953aa80ed00
Alberto Clemente <alberto.clemente@sisal.it>
2024-05-25 00:07:32 +0200

    removed default valures

--- variables.tf
+++ variables.tf
@@ -4,20 +4,16 @@ variable "compartment_ocid" {
 
 variable "ingress_cidrs" {
     type = string
-    default = "192.168.0.0/24"
 }
 
 variable "egress_cidrs" {
     type = string
-    default = "192.168.10.0/24"
 }
 
 variable "egress_ip_id" {
     type = string
-    default = null
 }
 
 variable "workload_cidrs" {
     type = string
-    default = "192.168.100.0/24"
 }
