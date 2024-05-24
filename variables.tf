1a5db4131b11d53c2d4953385662fc4868486d03
Alberto Clemente <alberto.clemente@sisal.it>
2024-05-25 00:19:26 +0200

    added description for input variables

--- variables.tf
+++ variables.tf
@@ -1,20 +1,25 @@
 variable "compartment_ocid" {
     type = string
+    description = "The OCID of the compartment"
 }
 
 variable "ingress_cidrs" {
     type = string
+    description = "List of CIDRs, space separated for ingress subnets"
 }
 
 variable "egress_cidrs" {
     type = string
+    description = "List of CIDRs, space separated for egress subnets"
 }
 
 variable "workload_cidrs" {
     type = string
+    description = "List of CIDRs, space separated for workload subnets"
 }
 
 variable "egress_ip_ocid" {
     type = string
     default = null
+    description = "Optional public IP OCID for NAT Gateway"
 }
