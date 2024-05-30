e2fd1908d191c2ecdf5a79ca93652649d39bdf78
Alberto Clemente <alberto.clemente@sisal.it>
2024-05-30 09:06:40 +0200

    fixed typo

--- modules/drg/outputs.tf
+++ modules/drg/outputs.tf
@@ -1,3 +1,3 @@
 output "rpcs" {
-  value = { for rpc in oci_coci_core_remote_peering_connection.hubs: rpc.id => rpc }
+  value = { for rpc in oci_core_remote_peering_connection.hubs: rpc.id => rpc }
 }
